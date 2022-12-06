5a5316ea3dee22551f1d7659147d58472cd87443
rhartsuiker <r.hartsuiker@st.hanze.nl>
2022-12-06 15:41:36 +0100

    Added db_utils package that contains classes which can read a config file for db connection info | added a basic sql connector that can read users from the user database table

---
+++ src/main/java/nl/bioinf/jabusker/portfolio/db_utils/DbCredentials.java
@@ -0,0 +1,52 @@
+package nl.bioinf.jabusker.portfolio.db_utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DbCredentials {
+    public static String getMySQLdbPassword() throws PasswordRetrievalException, FileNotFoundException, IOException {
+
+        DbUser u;
+        try {
+            u = getMySQLuser();
+        } catch (NoSuchFieldException e) {
+            throw new PasswordRetrievalException(e.getMessage());
+        }
+        return u.databasePassword;
+
+    }
+
+    /**
+     *
+     * @return @throws FileNotFoundException
+     * @throws IOException
+     * @throws NoSuchFieldException
+     */
+    public static DbUser getMySQLuser() throws FileNotFoundException, IOException, NoSuchFieldException {
+        String passPropsFileName = System.getProperty("user.home") + File.separator + ".my.cnf";
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(passPropsFileName));
+
+        DbUser u = new DbUser();
+        if (!properties.containsKey("user")) {
+            throw new NoSuchFieldException("field \"user\" is not found in the configuration");
+        }
+        u.userName = properties.getProperty("user");
+
+        if (!properties.containsKey("password")) {
+            throw new NoSuchFieldException("field \"password\" is not found in the configuration");
+        }
+        u.databasePassword = properties.getProperty("password");
+
+        if (!properties.containsKey("url")) {
+            throw new NoSuchFieldException("field \"url\" is not found in the configuration");
+        }
+        u.url = properties.getProperty("url");
+
+        return u;
+    }
+
+}
---
+++ src/main/java/nl/bioinf/jabusker/portfolio/db_utils/PasswordRetrievalException.java
@@ -0,0 +1,9 @@
+package nl.bioinf.jabusker.portfolio.db_utils;
+
+public class PasswordRetrievalException extends Exception {
+
+    public PasswordRetrievalException(String arg0) {
+        super(arg0);
+    }
+
+}
