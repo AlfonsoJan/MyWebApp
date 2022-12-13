96e942d3e44c60eb2f9fb297bc601ba5889ac4c5
rhartsuiker <r.hartsuiker@st.hanze.nl>
2022-12-13 15:11:43 +0100

    Added download functionality to the download button. Class DownloadServlet.

--- src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
+++ src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
@@ -7,7 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
 
 @WebServlet(name = "DownloadServlet", urlPatterns = "/download", loadOnStartup = 1)
@@ -22,9 +25,40 @@ public class DownloadServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String[] myJsonData = request.getParameterValues("json[]");
-        for (String i:
-                myJsonData) {
-            System.out.println(i);
+        for (String fileName: myJsonData) {
+
+            FileInputStream fileInputStream = null;
+            OutputStream responseOutputStream = null;
+
+            try
+            {
+                String filePath = request.getServletContext().getRealPath("/WEB-INF/resources/")+ fileName;
+                File file = new File(filePath);
+
+                String mimeType = request.getServletContext().getMimeType(filePath);
+                if (mimeType == null) {
+                    mimeType = "application/octet-stream";
+                }
+                response.setContentType(mimeType);
+                response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+                response.setContentLength((int) file.length());
+
+                fileInputStream = new FileInputStream(file);
+                responseOutputStream = response.getOutputStream();
+                int bytes;
+                while ((bytes = fileInputStream.read()) != -1) {
+                    responseOutputStream.write(bytes);
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            finally
+            {
+                fileInputStream.close();
+                responseOutputStream.close();
+            }
         }
     }
 }
