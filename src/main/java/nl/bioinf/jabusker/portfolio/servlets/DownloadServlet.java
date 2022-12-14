e36920843e6603048310b43fb75fbe2969b805b6
rhartsuiker <r.hartsuiker@st.hanze.nl>
2022-12-14 12:21:56 +0100

    DOWNLOADING WORKS!!!! (for now)

--- src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
+++ src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
@@ -1,27 +1,42 @@
 package nl.bioinf.jabusker.portfolio.servlets;
 
-import javax.servlet.ServletOutputStream;
+import nl.bioinf.jabusker.portfolio.model.TarGzipExample1;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @WebServlet(name = "DownloadServlet", urlPatterns = "/download", loadOnStartup = 1)
 public class DownloadServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int queryLength = request.getParameterValues("allFiles").length;
+        String[] allFiles = request.getParameterValues("allFiles");
 
-        for (String fileName: request.getParameterValues("allFiles")) {
-            System.out.println(fileName);
+        if (allFiles.length > 1) {
+            // remove this when we get the actual file location and add that to tarzip model
+            String resourcePath = getServletContext().getRealPath("") + "/WEB-INF/resources/";
+
+            TarGzipExample1 zipper = new TarGzipExample1(resourcePath, allFiles);
+
+            List<Path> paths = zipper.fileNamesToPaths();
+            Path outPath = Paths.get(resourcePath + "temp/out.tar.gz");
+
+            zipper.createTarGzipFiles(paths, outPath);
 
             FileInputStream fileInputStream = null;
             OutputStream responseOutputStream = null;
 
             try
             {
-                String filePath = request.getServletContext().getRealPath("/WEB-INF/resources/") + fileName;
+                String filePath = String.valueOf(outPath);
                 File file = new File(filePath);
 
                 String mimeType = request.getServletContext().getMimeType(filePath);
@@ -29,7 +44,7 @@ public class DownloadServlet extends HttpServlet {
                     mimeType = "application/octet-stream";
                 }
                 response.setContentType(mimeType);
-                response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+                response.addHeader("Content-Disposition", "attachment; filename=" + "out.tar.gz");
                 response.setContentLength((int) file.length());
 
                 fileInputStream = new FileInputStream(file);
@@ -49,5 +64,41 @@ public class DownloadServlet extends HttpServlet {
                 responseOutputStream.close();
             }
         }
+        else {
+            FileInputStream fileInputStream = null;
+            OutputStream responseOutputStream = null;
+
+            try
+            {
+                String filePath = request.getServletContext().getRealPath("/WEB-INF/resources/") + allFiles[0];
+                File file = new File(filePath);
+
+                String mimeType = request.getServletContext().getMimeType(filePath);
+                if (mimeType == null) {
+                    mimeType = "application/octet-stream";
+                }
+                response.setContentType(mimeType);
+                response.addHeader("Content-Disposition", "attachment; filename=" + allFiles[0]);
+                response.setContentLength((int) file.length());
+
+                fileInputStream = new FileInputStream(file);
+                responseOutputStream = response.getOutputStream();
+                int bytes;
+                while ((bytes = fileInputStream.read()) != -1) {
+                    responseOutputStream.write(bytes);
+                }
+            }
+            catch(Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            finally
+            {
+                fileInputStream.close();
+                responseOutputStream.close();
+            }
+        }
+
+
     }
 }
