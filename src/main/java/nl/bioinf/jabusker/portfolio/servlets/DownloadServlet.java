8ec9be0141c22567fadb5d7bf5f596362c8965ad
AlfonsoJan <alfonsobusker@gmail.com>
2022-12-14 12:10:50 +0100

    Updated the download servlet

--- src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
+++ src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
@@ -1,18 +1,18 @@
 package nl.bioinf.jabusker.portfolio.servlets;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 @WebServlet(name = "DownloadServlet", urlPatterns = "/download", loadOnStartup = 1)
 public class DownloadServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int queryLength = request.getParameterValues("allFiles").length;
+
         for (String fileName: request.getParameterValues("allFiles")) {
             System.out.println(fileName);
 
