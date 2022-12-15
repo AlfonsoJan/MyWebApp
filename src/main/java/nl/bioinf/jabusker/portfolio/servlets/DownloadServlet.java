cf46b83b1c7b906a4c71df701098ba548bb62e72
rhartsuiker <r.hartsuiker@st.hanze.nl>
2022-12-15 13:42:01 +0100

    Removed redundant code from DownloadServlet.

--- src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
+++ src/main/java/nl/bioinf/jabusker/portfolio/servlets/DownloadServlet.java
@@ -16,89 +16,61 @@ import java.util.List;
 
 @WebServlet(name = "DownloadServlet", urlPatterns = "/download", loadOnStartup = 1)
 public class DownloadServlet extends HttpServlet {
+    private String fileName;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String[] allFiles = request.getParameterValues("allFiles");
+        String resourcePath = request.getServletContext().getRealPath("/WEB-INF/resources/");
 
-        if (allFiles.length > 1) {
-            // remove this when we get the actual file location and add that to tarzip model
-            String resourcePath = getServletContext().getRealPath("") + "/WEB-INF/resources/";
+        if (allFiles.length == 1) {
+            fileName = allFiles[0];
+        }
+        else if (allFiles.length > 1) {
+            fileName = "placeholder.tar.gz";
 
             TarGzipExample1 zipper = new TarGzipExample1(resourcePath, allFiles);
 
+            resourcePath = resourcePath + "/temp";
+
             List<Path> paths = zipper.fileNamesToPaths();
-            Path outPath = Paths.get(resourcePath + "temp/out.tar.gz");
+            Path outPath = Paths.get(resourcePath + fileName);
 
             zipper.createTarGzipFiles(paths, outPath);
-
-            FileInputStream fileInputStream = null;
-            OutputStream responseOutputStream = null;
-
-            try
-            {
-                String filePath = String.valueOf(outPath);
-                File file = new File(filePath);
-
-                String mimeType = request.getServletContext().getMimeType(filePath);
-                if (mimeType == null) {
-                    mimeType = "application/octet-stream";
-                }
-                response.setContentType(mimeType);
-                response.addHeader("Content-Disposition", "attachment; filename=" + "out.tar.gz");
-                response.setContentLength((int) file.length());
-
-                fileInputStream = new FileInputStream(file);
-                responseOutputStream = response.getOutputStream();
-                int bytes;
-                while ((bytes = fileInputStream.read()) != -1) {
-                    responseOutputStream.write(bytes);
-                }
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-            }
-            finally
-            {
-                fileInputStream.close();
-                responseOutputStream.close();
-            }
-        }
-        else {
-            FileInputStream fileInputStream = null;
-            OutputStream responseOutputStream = null;
-
-            try
-            {
-                String filePath = request.getServletContext().getRealPath("/WEB-INF/resources/") + allFiles[0];
-                File file = new File(filePath);
-
-                String mimeType = request.getServletContext().getMimeType(filePath);
-                if (mimeType == null) {
-                    mimeType = "application/octet-stream";
-                }
-                response.setContentType(mimeType);
-                response.addHeader("Content-Disposition", "attachment; filename=" + allFiles[0]);
-                response.setContentLength((int) file.length());
-
-                fileInputStream = new FileInputStream(file);
-                responseOutputStream = response.getOutputStream();
-                int bytes;
-                while ((bytes = fileInputStream.read()) != -1) {
-                    responseOutputStream.write(bytes);
-                }
-            }
-            catch(Exception ex)
-            {
-                ex.printStackTrace();
-            }
-            finally
-            {
-                fileInputStream.close();
-                responseOutputStream.close();
-            }
         }
 
+        FileInputStream fileInputStream = null;
+        OutputStream responseOutputStream = null;
 
+        try
+        {
+            String filePath = resourcePath + fileName;
+            File file = new File(filePath);
+
+            String mimeType = request.getServletContext().getMimeType(filePath);
+            if (mimeType == null) {
+                mimeType = "application/octet-stream";
+            }
+            response.setContentType(mimeType);
+            response.addHeader("Content-Disposition", "attachment; filename=" + fileName);
+            response.setContentLength((int) file.length());
+
+            fileInputStream = new FileInputStream(file);
+            responseOutputStream = response.getOutputStream();
+            int bytes;
+            while ((bytes = fileInputStream.read()) != -1) {
+                responseOutputStream.write(bytes);
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            assert fileInputStream != null;
+            fileInputStream.close();
+            assert responseOutputStream != null;
+            responseOutputStream.close();
+        }
     }
 }
