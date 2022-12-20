e9e9af17a2082854ff35d31fe88c2c7450cb8675
Alfonso Jan <alfonsobusker@gmail.com>
2022-12-20 15:54:24 +0100

    Added a ajax call to get the projectname

--- src/main/java/nl/bioinf/jabusker/portfolio/servlets/CreateProjectServlet.java
+++ src/main/java/nl/bioinf/jabusker/portfolio/servlets/CreateProjectServlet.java
@@ -24,31 +24,31 @@ import java.util.concurrent.ThreadLocalRandom;
 public class CreateProjectServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
-        String[] files = request.getParameter("files").split(",");
-        boolean con = true;
+        String projectName = request.getParameterValues("projectname[]")[0];
+
+//        String name = request.getParameter("name");
+//        String[] files = request.getParameter("files").split(",");
+//        boolean con = true;
         try {
-            MockupServlet.getConnector().insertProject(name, 1);
+            MockupServlet.getConnector().insertProject(projectName, 1);
         } catch (DatabaseException e) {
-            System.out.println("H");
-            return;
-//            con = false;
+            throw new RuntimeException(e);
         }
-        int projectId;
-        if (con) {
-            try {
-                projectId = MockupServlet.getConnector().getProject(name, 1).getProjectId();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                for (String path : files) {
-                    MockupServlet.getConnector().insertLabeledFile(path, path, projectId);
-                }
-            } catch (DatabaseException e) {
-                throw new RuntimeException(e);
-            }
-        }
+//        int projectId;
+//        if (con) {
+//            try {
+//                projectId = MockupServlet.getConnector().getProject(name, 1).getProjectId();
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//            try {
+//                for (String path : files) {
+//                    MockupServlet.getConnector().insertLabeledFile(path, path, projectId);
+//                }
+//            } catch (DatabaseException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
 
     }
 }
